5c3335d56c19d26358c82286fd394b7f603848c3
rohitcodism <rohitpaula18i@gmail.com>
2023-07-21 08:13:01 +0530

    do it

--- mui-app/src/App.js
+++ mui-app/src/App.js
@@ -1,20 +1,11 @@
+import { Button } from '@mui/material'
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img  className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
     </div>
   );
 }
